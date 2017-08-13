ecab33fc768ba0b40a5bde3cd3380d91e18b34aa
Terrell Tullis <t.tullis34@outlook.com>
2017-08-13 13:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase){
     return "I can\'t hear you"
-  }else if (string === string.toUpperCase){
+  }else{
     return "YES INDEED!"
   }
 }
